06efb2e6541a15453fa9c3ba702bf64075c71dca
Jie Wang <wjidea@gmail.com>
2025-10-01 22:35:33 -0700

    update py script

--- lab4/parse_fasta_biopython.py
+++ lab4/parse_fasta_biopython.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import sys
 from Bio import SeqIO
-from Bio.SeqUtils import GC
+from Bio.SeqUtils import gc_fraction
 
 def parse_fasta_file(fasta_file_path):
     sequences = SeqIO.parse(fasta_file_path, "fasta")
     for seq_record in sequences:
-        print(seq_record.id, len(seq_record), round(GC(seq_record.seq), 2))
+        print(seq_record.id, len(seq_record), round(gc_fraction(seq_record.seq), 2))
 
 if __name__ == "__main__":
     parse_fasta_file(sys.argv[1])
--- lab4/parse_fasta_wo_biopython.py
+++ lab4/parse_fasta_wo_biopython.py
@@ -4,19 +4,22 @@ import sys
 
 def parse_fasta_file(fasta_file_path):
     fh = open(fasta_file_path)
-    first_record = True
     seq = ""
     seq_name=""
     for line in fh.readlines():
         if line.startswith('>'):
             if len(seq_name) > 0:
-                print(seq_name, len(seq))
+                print(seq_name, len(seq), gc_fraction(seq))
             seq_name = line.split(' ')[0][1:]
             seq = ""
         else:
             seq += line.rstrip()
-    print(seq_name, len(seq))
+    print(seq_name, len(seq), gc_fraction(seq))
     fh.close()
+
+def gc_fraction(seq):
+    GC_bases = seq.count('G') + seq.count('g') + seq.count('C') + seq.count('c')
+    return round(GC_bases/len(seq), 2)
 
 if __name__ == "__main__":
     parse_fasta_file(sys.argv[1])
